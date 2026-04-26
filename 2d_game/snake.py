68de120b2d0743aca0b3faa1de79f44ee1d97d27
blu <blu@casu.blub.de>
2026-04-26 14:59:21 +0200

    finished running snake game and first round of tests

--- 2d_game/snake.py
+++ 2d_game/snake.py
@@ -1,13 +1,49 @@
+'''
+##########################################################################################
+    Y
+  .-^-.
+ /     \      .- ~ ~ -.
+()     ()    /   _ _   `.                     _ _ _
+ \_   _/    /  /     \   \                . ~  _ _  ~ .
+   | |     /  /       \   \             .' .~       ~-. `.
+   | |    /  /         )   )           /  /             `.`.
+   \ \_ _/  /         /   /           /  /                `'
+    \_ _ _.'         /   /           (  (
+                    /   /             \  \
+                   /   /               \  \
+                  /   /                 )  )
+                 (   (                 /  /
+                  `.  `.             .'  /
+                    `.   ~ - - - - ~   .'
+                       ~ . _ _ _ _ . ~
+
+
+https://www.asciiart.eu/art/b12656730a00300c
+##########################################################################################
+How to play:
+1. Start the DIPPID app on your phone, set it to your_ip:5700 and start sending data
+2. Hold your phone horizontal with the short side pointing to the front.
+3. Start this script
+4. Tilt your phone to the front/back or to the sides to change the direction of the snake.
+5. Have fun!
+##########################################################################################
+'''
+
 import pyglet
 from pyglet import window, shapes, font
 from DIPPID import SensorUDP
 import random
 import time
+import sys
 
+#window height/width have to be multiples of the square size
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 PORT = 5700
 SQUARE_SIZE = 20
+
+#delay between snake movements. Decrease to increase the speed
+delay = 0.25
 
 sensor = SensorUDP(PORT)
 
@@ -24,12 +60,52 @@ end = False
 #shapes
 head = shapes.Rectangle(400, 400, SQUARE_SIZE, SQUARE_SIZE, (0, 100, 50))
 body = shapes.Rectangle(400, 400, SQUARE_SIZE, SQUARE_SIZE, (0, 150, 100))
-food = shapes.Rectangle(random.randint(0, (WINDOW_WIDTH - SQUARE_SIZE)), random.randint(0, (WINDOW_HEIGHT - SQUARE_SIZE)), SQUARE_SIZE, SQUARE_SIZE, (200, 0, 0))
+#food position has to be in the window and a multiple of SQUARE_SIZE, so the snake head can match the position
+food = shapes.Rectangle(random.randint(0, int((WINDOW_WIDTH - SQUARE_SIZE)/SQUARE_SIZE)) * SQUARE_SIZE, random.randint(0, int((WINDOW_HEIGHT - SQUARE_SIZE)/SQUARE_SIZE)) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, (200, 0, 0))
 
 #coordinated of body segments
-#FIXME should be empty at the start, just for testing
-segments = [[380, 400], [360, 400], [340, 400]]
+segments = [[380, 400], [360, 400], [340, 400]] #inital snake body
 
+#get position for the next food that is not occupied by the snake
+def get_new_food_position():
+    x = random.randint(0, int((WINDOW_WIDTH - SQUARE_SIZE)/SQUARE_SIZE)) * SQUARE_SIZE
+    y = random.randint(0, int((WINDOW_HEIGHT - SQUARE_SIZE)/SQUARE_SIZE)) * SQUARE_SIZE
+
+    #check head
+    if (head.x == x and head.y == y):
+        return(get_new_food_position())
+    else:
+        #check body
+        for i in range(len(segments)):
+            if (segments[i][0] == x and segments[i][1] == y):
+                return(get_new_food_position())
+        print(f"food position: {x},{y}") #FIXME testing
+        return(x, y) #no collision detected until here
+
+#add new body segment at the end of the snake
+def add_body_segment():
+    if segments == []:
+        prevx = head.x
+        prevy = head.y
+    else:
+        prevx = body.x
+        prevy = body.y
+    
+    x = prevx
+    y = prevy
+    
+    if direction == "D":
+        y = prevy + SQUARE_SIZE 
+    elif direction == "U":
+        y = prevy - SQUARE_SIZE
+    elif direction == "L":
+        x = prevx + SQUARE_SIZE
+    elif direction == "R":
+        x = prevx - SQUARE_SIZE
+
+    segments.append([x,y])
+
+    
 
 #play
 @win.event
@@ -39,13 +115,15 @@ def on_draw():
     global segments
     global score
     global end
-    #FIXME add some?
+    global delay
 
     if not end:
 
         win.clear()
 
-        #update body positions: #TODO test
+        food.draw()
+
+        #update body positions:
         #each takes the position of the one in front, the first takes the position of the head
         print(f"segments: {segments}")
         for i in range(len(segments)-1, 0, -1):
@@ -67,13 +145,19 @@ def on_draw():
 
             #Threshold for smooter operation
             #snake can only move in one direction at once and keeps direction on no input
-            if(sensor.get_value('gravity')['y'] > 1):
+            #check if the movement is the one from before so we don't accidentally overwrite the next movement
+            #in case the user does not tilt the device back to horizontal 
+
+            #people kept intuitively tilting the phone to the back and running into themselves.
+            #adjusted y threshold accordingly 
+            if(sensor.get_value('gravity')['y'] > 5 and not direction == "D"):
+                
                 direction = "D"
-            elif(sensor.get_value('gravity')['y'] < -1):
+            if(sensor.get_value('gravity')['y'] < -2 and not direction == "U"):
                 direction = "U"
-            elif(sensor.get_value('gravity')['x'] > 1):
+            if(sensor.get_value('gravity')['x'] > 2 and not direction == "L"):
                 direction = "L"
-            elif(sensor.get_value('gravity')['x'] < -1):
+            if(sensor.get_value('gravity')['x'] < -2 and not direction == "R"):
                 direction = "R"
             
         if direction == "D":
@@ -86,6 +170,13 @@ def on_draw():
             head.x += SQUARE_SIZE
 
         #collisions
+        #food
+        if(head.x == food.x and head.y == food.y):
+            score += 1
+            food.x, food.y = get_new_food_position()
+            add_body_segment()
+            
+
         #wall
         if(head.x < 0 or head.x == WINDOW_WIDTH or head.y < 0 or head.y == WINDOW_HEIGHT):
             end = True
@@ -107,21 +198,10 @@ def on_draw():
             head.draw()  
         
     #draw the window contents/move once every second
-    time.sleep(1)
-            
-
+    time.sleep(delay)
 
 pyglet.app.run()
 
-#TODO
-#boundaries (modulo or end?)
-#end conditions
-#end screen
-#food appearance
-#food consumption
-
-#FIXME
-#boundaries
-#head can overtake body (direction change weird?)
-#positioning of body parts?
-
+#TODO:
+#add play again button
+#?fix weird warning bc of ascii art?
