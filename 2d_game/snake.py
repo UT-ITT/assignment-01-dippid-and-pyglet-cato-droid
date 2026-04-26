86deb35925ac8e15208f2fa3bd8c28b623162d21
blu <blu@casu.blub.de>
2026-04-26 13:05:21 +0200

    fixed list assignment error, added collisions and ending + cool font

--- 2d_game/snake.py
+++ 2d_game/snake.py
@@ -1,5 +1,5 @@
 import pyglet
-from pyglet import window, shapes
+from pyglet import window, shapes, font
 from DIPPID import SensorUDP
 import random
 import time
@@ -13,6 +13,9 @@ sensor = SensorUDP(PORT)
 
 win = window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 
+#load cool font (https://www.1001fonts.com/pixel-game-font.html)
+font.add_file('Pixel Game.otf')
+
 #initial values
 score = 0
 direction = "R" #one of "R", "L", "U", "D"
@@ -22,11 +25,10 @@ end = False
 head = shapes.Rectangle(400, 400, SQUARE_SIZE, SQUARE_SIZE, (0, 100, 50))
 body = shapes.Rectangle(400, 400, SQUARE_SIZE, SQUARE_SIZE, (0, 150, 100))
 food = shapes.Rectangle(random.randint(0, (WINDOW_WIDTH - SQUARE_SIZE)), random.randint(0, (WINDOW_HEIGHT - SQUARE_SIZE)), SQUARE_SIZE, SQUARE_SIZE, (200, 0, 0))
-endscreen = ...#FIXME
 
 #coordinated of body segments
 #FIXME should be empty at the start, just for testing
-segments = [[420, 400], [440, 400], [460, 400]]
+segments = [[380, 400], [360, 400], [340, 400]]
 
 
 #play
@@ -35,61 +37,76 @@ def on_draw():
     #global variables we want to use in this function
     global direction
     global segments
+    global score
+    global end
     #FIXME add some?
 
-    win.clear()
-    # check if sensor data is received
-    if(not sensor.get_capabilities() == [] and not end):
+    if not end:
+
+        win.clear()
+
         #update body positions: #TODO test
         #each takes the position of the one in front, the first takes the position of the head
-        print(f"segments: {segments}")#FIXME error with assignments
+        print(f"segments: {segments}")
         for i in range(len(segments)-1, 0, -1):
-            print(i)
-            temp = segments[i-1]
-            segments[i] = temp #FIXME
+            tempx = segments[i-1][0]
+            tempy = segments[i-1][1]
+            segments[i] = [tempx, tempy]
             body.x = segments[i][0]
             body.y = segments[i][1]
             body.draw()
-            print(f"b{i}({body.x},{body.y})")#FIXME
-            print(f"segments: {segments}")#FIXME
         if not segments == []:
             segments[0][0] = head.x
             segments[0][1] = head.y
             body.x = head.x
             body.y = head.y
             body.draw()
-            print(f"b0({body.x},{body.y})")#FIXME
-            print(f"segments: {segments}")#FIXME why is b1 changed here?????
 
+        # check if sensor data is received
+        if not sensor.get_capabilities() == []:
 
+            #Threshold for smooter operation
+            #snake can only move in one direction at once and keeps direction on no input
+            if(sensor.get_value('gravity')['y'] > 1):
+                direction = "D"
+            elif(sensor.get_value('gravity')['y'] < -1):
+                direction = "U"
+            elif(sensor.get_value('gravity')['x'] > 1):
+                direction = "L"
+            elif(sensor.get_value('gravity')['x'] < -1):
+                direction = "R"
+            
+        if direction == "D":
+            head.y -= SQUARE_SIZE
+        elif direction == "U":
+            head.y  += SQUARE_SIZE
+        elif direction == "L":
+            head.x -= SQUARE_SIZE
+        elif direction == "R":
+            head.x += SQUARE_SIZE
 
-        #Threshold for smooter operation
-        #snake can only move in one direction at once and keeps direction on no input
-        if(sensor.get_value('gravity')['y'] > 1):
-            direction = "U"
-        elif(sensor.get_value('gravity')['y'] < -1):
-            direction = "D"
-        elif(sensor.get_value('gravity')['x'] > 1):
-            direction = "R"
-        elif(sensor.get_value('gravity')['x'] < -1):
-            direction = "L"
+        #collisions
+        #wall
+        if(head.x < 0 or head.x == WINDOW_WIDTH or head.y < 0 or head.y == WINDOW_HEIGHT):
+            end = True
+
+        #body parts
+        for i in range(len(segments)):
+            if (head.x == segments[i][0] and head.y == segments[i][1]):
+                end = True
+
+        #end snake movement and show Game Over + Score
+        if end:
+            label = pyglet.text.Label(f'Game Over!  Score: {score}',
+                            font_name='Pixel Game',
+                            font_size=36,
+                            x=10, y=10)
+            label.draw()
+        else:
+            #draw head if it didn't collide with anything
+            head.draw()  
         
-    if direction == "U":
-        head.y -= SQUARE_SIZE
-    elif direction == "D":
-        head.y  += SQUARE_SIZE
-    elif direction == "R":
-        head.x -= SQUARE_SIZE
-    elif direction == "L":
-        head.x += SQUARE_SIZE
-
-    print(f"h({head.x},{head.y})") #FIXME
-    head.draw()
-
-    if end:
-        endscreen.draw()
-    
-    #move every second
+    #draw the window contents/move once every second
     time.sleep(1)
             
 
@@ -106,4 +123,5 @@ pyglet.app.run()
 #FIXME
 #boundaries
 #head can overtake body (direction change weird?)
+#positioning of body parts?
 
